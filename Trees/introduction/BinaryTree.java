b171b324855d48f42cf41b3603809a4be392da6a
shoaibkhan1s <170645833+shoaibkhan1s@users.noreply.github.com>
2026-06-02 13:42:11 +0530

    Binary tree

--- Trees/introduction/BinaryTree.java
+++ Trees/introduction/BinaryTree.java
@@ -6,13 +6,15 @@ public class BinaryTree {
 
   public BinaryTree() {
   }
+
+  
   private static class Node {
     int value;
     Node left;
     Node right;
 
 
-    
+
     public Node(int value) {
       this.value = value;
     }
